f8412d1e308fb76881890441abd45d8eb44a56f8
Miroslav Rešetar <mresetar@croz.net>
2020-05-19 17:06:42 +0200

    Fixing docker volume mount

--- deploy-to-azure.sh
+++ deploy-to-azure.sh
@@ -5,8 +5,12 @@ function main() {
   local username=$1
   local tenant=$2
   local pem=$3
+  # Setup Windows
+  # export WIN_LOCAL_DIR=/c/projects/circleci-demo-java-spring/
+  local current_dir=${WIN_LOCAL_DIR:-$(pwd)}
+  local localdir=${CIRCLE_WORKING_DIRECTORY:-$current_dir}
   docker run -it -e "AZ_USERNAME=$username" -e "AZ_TENANT=$tenant" -e "AZ_PEM=$pem" --name circle-ci-azure --rm \
-    -v $(pwd):/app -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "${localdir}:/app" -v /var/run/docker.sock:/var/run/docker.sock \
     --entrypoint /app/deploy.sh mresetar/openjdk11-gradle551-az251:latest
 }
 
