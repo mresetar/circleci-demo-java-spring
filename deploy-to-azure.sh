81089af512858cc7f9974aa017a67a9c681a3502
Miroslav Rešetar <mresetar@croz.net>
2020-05-19 13:32:22 +0200

    Fixing deploy

--- deploy-to-azure.sh
+++ deploy-to-azure.sh
@@ -6,11 +6,8 @@ function main() {
   local tenant=$2
   local pem=$3
   docker run -it -e "AZ_USERNAME=$username" -e "AZ_TENANT=$tenant" -e "AZ_PEM=$pem" --name circle-ci-azure --rm \
-    -v /c/projects/circleci-demo-java-spring/:/app -v /var/run/docker.sock:/var/run/docker.sock \
+    -v .:/app -v /var/run/docker.sock:/var/run/docker.sock \
     --entrypoint /app/deploy.sh mresetar/openjdk11-gradle551-az251:latest
 }
 
-for i; do 
-  echo $i 
-done
 main "$@"
