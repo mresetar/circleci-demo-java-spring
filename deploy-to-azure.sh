d2ba42b1bb641ab568ad550fa8c845520d8cb184
Miroslav Rešetar <mresetar@croz.net>
2020-05-19 17:16:48 +0200

    Fixing circleci expansion

--- deploy-to-azure.sh
+++ deploy-to-azure.sh
@@ -8,6 +8,8 @@ function main() {
   # Setup Windows
   # export WIN_LOCAL_DIR=/c/projects/circleci-demo-java-spring/
   local current_dir=${WIN_LOCAL_DIR:-$(pwd)}
+  # Fix CIRCLE working dir expansion (https://discuss.circleci.com/t/circle-working-directory-doesnt-expand/17007/5)
+  CIRCLE_WORKING_DIRECTORY="${CIRCLE_WORKING_DIRECTORY/#\~/$HOME}"
   local localdir=${CIRCLE_WORKING_DIRECTORY:-$current_dir}
   docker run -it -e "AZ_USERNAME=$username" -e "AZ_TENANT=$tenant" -e "AZ_PEM=$pem" --name circle-ci-azure --rm \
     -v "${localdir}:/app" -v /var/run/docker.sock:/var/run/docker.sock \
